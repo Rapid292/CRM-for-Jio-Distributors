7b9261366a8212b46071a48442db8983f6e11583
Rishabh Gupta <gupta.rishabh2912@gmail.com>
2020-06-25 18:51:57 +0530

    Mail change

--- crm/config.py
+++ crm/config.py
@@ -2,10 +2,11 @@ import os
 
 
 class Config:
-   SECRET_KEY = "e9e14493faed51435c3011ba8cbc61b1"
-   SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-   MAIL_SERVER = "smtp.googlemail.com"
-   MAIL_PORT = 587
-   MAIL_USE_TLS = True
-   MAIL_USERNAME = os.environ.get("EMAIL_USER")
-   MAIL_PASSWORD = os.environ.get("EMAIL_PASS")
+    SECRET_KEY = os.environ.get("SIDH_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SIDH_SQLALCHEMY_DATABASE_URI")
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("SIDH_EMAIL_USER")
+    MAIL_PASSWORD = os.environ.get("SIDH_EMAIL_PASS")
+
